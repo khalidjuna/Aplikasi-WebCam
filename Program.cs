26b594f4c7bfbefb8950b42200f66d42c4d8644e
khalidjuna <khalidjunata@gmail.com>
2023-05-26 14:44:57 +0700

    Bismillah

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using OpenCvSharp;
+
+namespace WebcamApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (VideoCapture capture = new VideoCapture(0))
+            {
+                using (Mat frame = new Mat())
+                {
+                    using (Window window = new Window("Webcam", WindowFlags.AutoSize))
+                    {
+                        while (true)
+                        {
+                            capture.Read(frame);
+
+                            if (frame.Empty())
+                                break;
+
+                            Cv2.ImShow("Webcam", frame);
+
+                            if (Cv2.WaitKey(1) == (int) 'q')
+                                break;
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
